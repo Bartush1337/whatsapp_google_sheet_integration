84b4eb693e38ae8d334b0e3c5685cf2dd41534a9
Guy Sharir <guysharir@gmail.com>
2023-10-11 14:20:22 -0700

    fix insert

--- google_sheet_pusher.py
+++ google_sheet_pusher.py
@@ -2,13 +2,14 @@ from google.oauth2 import service_account
 import gspread
 import json
 
-class SpreadSheetCommunicator():
+
+class SpreadSheetCommunicator:
     def __init__(self, config):
         service_account.Credentials.from_service_account_file(
-            config['permissions_file'],
-            scopes=['https://www.googleapis.com/auth/spreadsheets'],
+            config["permissions_file"],
+            scopes=["https://www.googleapis.com/auth/spreadsheets"],
         )
-        gc = gspread.service_account(filename=config['permissions_file'])
+        gc = gspread.service_account(filename=config["permissions_file"])
         self.sheet = gc.open_by_url(config["spreadsheet_url"])
 
     def next_available_row(self, worksheet):
@@ -16,18 +17,47 @@ class SpreadSheetCommunicator():
         return str(len(str_list) + 1)
 
     def communicate_message(self, message):
-        if 'driver' in message.keys():
-            worksheet = self.sheet.worksheet('drivers')
-            del message['driver']
-        elif 'passanger' in message.keys():
-            worksheet = self.sheet.worksheet('passangers')
-            del message['passanger']
-        elif 'error' in message.keys() :
-            worksheet = self.sheet.worksheet('other')
-            del message['error']
+        case = ""
+        if "driver" in message.keys():
+            worksheet = self.sheet.worksheet("drivers")
+            case = "driver"
+            del message["driver"]
+
+        elif "passanger" in message.keys():
+            worksheet = self.sheet.worksheet("passangers")
+            case = "passenger"
+            del message["passanger"]
+
+        elif "error" in message.keys():
+            worksheet = self.sheet.worksheet("other")
+            del message["error"]
+
         print(list(message.values()))
+
+        values = []
+        if case == "driver":
+            values = [
+                "נהג פעיל",
+                message["name"],
+                message["number"],
+                message["start_location"],
+                message["end_location"],
+                "",
+                message["date"],
+                message["time"],
+            ]
+        elif case == "passenger":
+            values = [
+                "מחכה לטיפול",
+                message["name"],
+                message["number"],
+                message["start_location"],
+                message["end_location"],
+                message["date"],
+                message["time"],
+            ]
+
         worksheet.append_row(
-            list(message.values()), table_range=f"A{self.next_available_row(worksheet)}"
+            values,
+            table_range=f"A{self.next_available_row(worksheet)}",
         )
-
-
