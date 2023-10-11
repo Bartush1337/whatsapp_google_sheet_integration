c2b153f0560356c811041c355e51b39d0dc83182
Ohad Barta <sohadb1357@gmail.com>
2023-10-11 22:55:01 +0300

    return 406 on bad parsing

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import http.client
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import google_sheet_pusher
@@ -10,8 +11,8 @@ config = json.load(open("config.json", "rb"))
 sheet_pusher = google_sheet_pusher.SpreadSheetCommunicator(config)
 
 class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
+    def _set_response(self, response=200):
+        self.send_response(response)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
@@ -26,20 +27,24 @@ class S(BaseHTTPRequestHandler):
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
         body = json.loads(post_data.decode('utf-8'))
+        response = http.client.OK
         if body["group_name"] in interesting_groups:
-            self.handle_message(body["message"])
+            response = self.handle_message(body["message"])
         else:
             print("group {} not interesting".format(body["group_name"]))
-        self._set_response()
+        self._set_response(response)
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
     def handle_message(self, message):
+        response = http.client.OK
         try:
             message_dict = par.pattern_match(message)
         except Exception:
             message_dict = {'error': True, 'raw_data' : message}
+            response = http.client.NOT_ACCEPTABLE
         logging.info(f"Parsed message: {message_dict}")
         sheet_pusher.communicate_message(message_dict)
+        return response
 
 def run(server_class=HTTPServer, handler_class=S, port=2001):
     logging.basicConfig(level=logging.INFO)
