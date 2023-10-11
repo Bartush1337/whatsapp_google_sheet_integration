c674091ad6d1fdbf7a62c99ee64acf9851638029
Ohad Barta <sohadb1357@gmail.com>
2023-10-11 22:25:12 +0300

    append always to the right place

--- google_sheet_pusher.py
+++ google_sheet_pusher.py
@@ -11,6 +11,10 @@ class SpreadSheetCommunicator():
         gc = gspread.service_account(filename=config['permissions_file'])
         self.sheet = gc.open_by_url(config["spreadsheet_url"])
 
+    def next_available_row(self, worksheet):
+        str_list = list(filter(None, worksheet.col_values(1)))
+        return str(len(str_list) + 1)
+
     def communicate_message(self, message):
         if 'driver' in message.keys():
             worksheet = self.sheet.worksheet('drivers')
@@ -22,6 +26,8 @@ class SpreadSheetCommunicator():
             worksheet = self.sheet.worksheet('other')
             del message['error']
         print(list(message.values()))
-        worksheet.append_rows([list(message.values())])
+        worksheet.append_row(
+            list(message.values()), table_range=f"A{self.next_available_row(worksheet)}"
+        )
 
 
